2f11be985502f509e84c8742af702b2b2deeefe6
ryan.o'rourke <ryan.o'rourke@kainos.com>
2023-05-16 14:42:04 +0100

    remove variables in app

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,9 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        String userPrompt;
-        String filePathForFolder;
-
         System.out.println( "Hello World!" );
     }
 }
