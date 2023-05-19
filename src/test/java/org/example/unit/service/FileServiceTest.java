1339864b3f460e0dd5b1d079e1522ce6d68f4fcb
ryan.o'rourke <ryan.o'rourke@kainos.com>
2023-05-19 14:38:30 +0100

    B004 add json, css, js and html test cases

--- src/test/java/org/example/unit/service/FileServiceTest.java
+++ src/test/java/org/example/unit/service/FileServiceTest.java
@@ -155,4 +155,129 @@ public class FileServiceTest {
                 textFile.delete();
         }
     }
+
+    @Test
+    public void testCssFileCreation() throws IOException, DuplicatedFileException {
+        final String fileName = "test1234.css";
+
+        final FileService fileService = new FileService("");
+        final File cssFile = fileService.createFile(fileName, "");
+
+        try {
+            assertNotNull(cssFile);
+            assertTrue(cssFile.isFile());
+            assertEquals(fileName, cssFile.getName());
+        } finally {
+            // Clean up
+            if (cssFile != null && cssFile.exists())
+                cssFile.delete();
+        }
+    }
+
+    @Test
+    public void testCssFileCreationWithContent() throws IOException, DuplicatedFileException {
+        final String fileContent = "body { color: blue; }";
+        final String fileName = "test1234.css";
+
+        final FileService fileService = new FileService("");
+        final File cssFile = fileService.createFile(fileName, fileContent);
+
+        try {
+            assertNotNull(cssFile);
+            assertTrue(cssFile.isFile());
+            assertEquals(fileName, cssFile.getName());
+
+            final String contentFromFile = Files.readString(cssFile.toPath());
+            assertEquals(fileContent, contentFromFile);
+        } finally {
+            // Clean up
+            if (cssFile != null && cssFile.exists())
+                cssFile.delete();
+        }
+    }
+
+    @Test
+    public void testJsonFileCreation() throws IOException, DuplicatedFileException {
+        final String fileName = "test1234.json";
+
+        final FileService fileService = new FileService("");
+        final File jsonFile = fileService.createFile(fileName, "");
+
+        try {
+            assertNotNull(jsonFile);
+            assertTrue(jsonFile.isFile());
+            assertEquals(fileName, jsonFile.getName());
+        } finally {
+            // Clean up
+            if (jsonFile != null && jsonFile.exists())
+                jsonFile.delete();
+        }
+    }
+
+    @Test
+    public void testJsonFileCreationWithContent() throws IOException, DuplicatedFileException {
+        final String fileContent = "{ \"key\": \"value\" }";
+        final String fileName = "test1234.json";
+
+        final FileService fileService = new FileService("");
+        final File jsonFile = fileService.createFile(fileName, fileContent);
+
+        try {
+            assertNotNull(jsonFile);
+            assertTrue(jsonFile.isFile());
+            assertEquals(fileName, jsonFile.getName());
+
+            final String contentFromFile = Files.readString(jsonFile.toPath());
+            assertEquals(fileContent, contentFromFile);
+        } finally {
+            // Clean up
+            if (jsonFile != null && jsonFile.exists())
+                jsonFile.delete();
+        }
+    }
+
+    @Test
+    public void testJavaScriptFileCreation() throws IOException, DuplicatedFileException {
+        final String fileName = "test1234.js";
+
+        final FileService fileService = new FileService("");
+        final File jsFile = fileService.createFile(fileName, "");
+
+        try {
+            assertNotNull(jsFile);
+            assertTrue(jsFile.isFile());
+            assertEquals(fileName, jsFile.getName());
+        } finally {
+            // Clean up
+            if (jsFile != null && jsFile.exists())
+                jsFile.delete();
+        }
+    }
+
+    @Test
+    public void testJavaScriptFileCreationWithContent() throws IOException, DuplicatedFileException {
+        final String fileContent = "function greet() {\n" +
+                "    console.log('Hello, World!');\n" +
+                "}\n" +
+                "\n" +
+                "greet();";
+
+        final String fileName = "test1234.js";
+
+        final FileService fileService = new FileService("");
+        final File jsFile = fileService.createFile(fileName, fileContent);
+
+        try {
+            assertNotNull(jsFile);
+            assertTrue(jsFile.isFile());
+            assertEquals(fileName, jsFile.getName());
+
+            final String contentFromFile = Files.readString(jsFile.toPath());
+            assertEquals(fileContent, contentFromFile);
+        } finally {
+            // Clean up
+            if (jsFile != null && jsFile.exists())
+                jsFile.delete();
+        }
+    }
 }
