995434d75cf5eaa3c52f0fd775695c36111a551c
ryan.o'rourke <ryan.o'rourke@kainos.com>
2023-06-08 15:51:03 +0100

    B019 Add Json validator

--- src/main/java/org/example/validators/AzureValidation.java
+++ src/main/java/org/example/validators/AzureValidation.java
@@ -1,2 +1,12 @@
-package org.example.validators;public class AzureValidation {
+package org.example.validators;
+
+public record AzureValidation(boolean isPass, String reason) {
+
+    @Override
+    public String reason() {
+        if (isPass())
+            return "Valid Requirement Passed";
+
+        return reason;
+    }
 }
